bb7b2790d62f9d4b9da35bda3614c65770f83398
Liza Belonina <fkme.fuhua@gmail.com>
2024-09-30 20:47:17 +0400

    Переименовала страницы

--- index.php
+++ index.php
@@ -5,9 +5,17 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <meta charset="utf-8">
         <title>Login</title>
+        <style>
+            body {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                min-height: 100vh;
+            }
+        </style>
     </head>
     <body>
-        <div class="justify-content-center align-items-center">
+        <div>
             <form action="authorize.php" method="post">
                 <h4>Wuthering Waves rule 34</h4>
                 <p>
